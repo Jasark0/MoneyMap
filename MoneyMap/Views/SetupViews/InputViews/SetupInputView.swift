cf9f2a2cbbb92dd6bbeeeb04e28113a0a01fe7cf
Chung-Hwa Yen <jasonyen2004@gmail.com>
2025-10-09 09:52:41 -0500

    feat: set up page frontend complete + added forgot your password button.

---
+++ MoneyMap/Views/SetupViews/InputViews/SetupInputView.swift
@@ -0,0 +1,18 @@
+//
+//  SetupInputView.swift
+//  MoneyMap
+//
+//  Created by user279040 on 10/9/25.
+//
+
+import SwiftUI
+
+struct SetupInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetupInputView()
+}
