838737a656153c451a9b6b044caceb6c55f8ab1c
sneha <jacobsneha949@gmail.com>
2025-10-17 11:54:47 -0500

    Add Xcode project files

---
+++ MoneyMap/MoneyMap/Views/SetupViews/ForgotPasswordView.swift
@@ -0,0 +1,19 @@
+//
+//  ForgotPasswordView.swift
+//  MoneyMap
+//
+//  Created by user279040 on 10/6/25.
+//
+
+import SwiftUI
+
+struct ForgotPasswordView: View {
+    var body: some View {
+        Text("Forgot Password Page")
+            .font(.system(size: 25))
+    }
+}
+
+#Preview {
+    ForgotPasswordView()
+}
